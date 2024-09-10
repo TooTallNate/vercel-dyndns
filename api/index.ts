ffcb103a72ac897010d6318a2366de952b000c97
Nathan Rajlich <n@n8.io>
2024-09-10 16:17:56 -0700

    Simplify code. More consistent error messages

--- api/index.ts
+++ api/index.ts
@@ -4,171 +4,141 @@ import basicAuth from 'basic-auth';
 const API = 'https://api.vercel.com/';
 
 export async function GET(request: Request) {
-    const url = new URL(request.url);
+	const url = new URL(request.url);
 
-    const hostname = url.searchParams.get('hostname');
-    if (!hostname) {
-        return new Response('no hostname', {
-            status: 400,
-            headers: {
-                'content-type': 'text/plain',
-            },
-        });
-    }
+	const hostname = url.searchParams.get('hostname');
+	if (!hostname) {
+		console.log('hostname query parameter not provided');
+		return res('notfqdn');
+	}
 
-    const ip = url.searchParams.get('myip');
-    if (!ip) {
-        return new Response('no ip', {
-            status: 400,
-            headers: {
-                'content-type': 'text/plain',
-            },
-        });
-    }
+	const parsedPsl = psl.parse(hostname);
+	if (parsedPsl.error) {
+		console.log(
+			`invalid hostname: ${parsedPsl.error.message} (${parsedPsl.error.code})`,
+		);
+		return res('notfqdn');
+	}
 
-    const auth = request.headers.get('authorization');
-    if (!auth) {
-        return new Response('no auth', {
-            status: 401,
-            headers: {
-                'content-type': 'text/plain',
-            },
-        });
-    }
+	if (!parsedPsl.domain) {
+		console.log('invalid hostname: no domain');
+		return res('notfqdn');
+	}
 
-    const parsedAuth = basicAuth.parse(auth);
-    if (!parsedAuth) {
-        return new Response('bad auth', {
-            status: 401,
-            headers: {
-                'content-type': 'text/plain',
-            },
-        });
-    }
+	if (!parsedPsl.subdomain) {
+		console.log('invalid hostname: no subdomain');
+		return res('notfqdn');
+	}
 
-    const parsedPsl = psl.parse(hostname);
-    //console.log({ parsedPsl });
+	const ip = url.searchParams.get('myip');
+	if (!ip) {
+		console.log('myip query parameter not provided');
+		return res('badrequest');
+	}
 
-    if (parsedPsl.error) {
-        return new Response(
-            `bad hostname: ${parsedPsl.error.message} (${parsedPsl.error.code})`,
-            {
-                status: 400,
-                headers: {
-                    'content-type': 'text/plain',
-                },
-            },
-        );
-    }
+	const auth = request.headers.get('authorization');
+	if (!auth) {
+		console.log('authorization header not provided');
+		return res('badauth');
+	}
 
-    if (!parsedPsl.domain) {
-        return new Response('bad hostname: no domain', {
-            status: 400,
-            headers: {
-                'content-type': 'text/plain',
-            },
-        });
-    }
+	const parsedAuth = basicAuth.parse(auth);
+	if (!parsedAuth) {
+		console.log('invalid authorization header');
+		return res('badauth');
+	}
 
-    if (!parsedPsl.subdomain) {
-        return new Response('bad hostname: no subdomain', {
-            status: 400,
-            headers: {
-                'content-type': 'text/plain',
-            },
-        });
-    }
+	const result = await update(
+		parsedAuth.pass,
+		parsedAuth.name,
+		parsedPsl.subdomain,
+		parsedPsl.domain,
+		ip,
+	);
 
-    const result = await update(
-        parsedAuth.pass,
-        parsedAuth.name,
-        parsedPsl.subdomain,
-        parsedPsl.domain,
-        ip,
-    );
+	return res(result);
+}
 
-    return new Response(result, {
-        headers: {
-            'content-type': 'text/plain',
-        },
-    });
+function res(body: string) {
+	return new Response(body, {
+		headers: {
+			'content-type': 'text/plain',
+		},
+	});
 }
 
 async function update(
-    token: string,
-    teamId: string,
-    hostname: string,
-    domain: string,
-    ip: string,
+	token: string,
+	teamId: string,
+	hostname: string,
+	domain: string,
+	ip: string,
 ): Promise<string> {
-    const GET_DNS = new URL(
-        `/v4/domains/${encodeURIComponent(domain)}/records?teamId=${teamId}&limit=100`,
-        API,
-    );
-    const res = await fetch(GET_DNS, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    if (!res.ok) {
-        return 'badauth';
-    }
-    const data = await res.json();
+	const headers = new Headers({ Authorization: `Bearer ${token}` });
 
-    // TODO: handle pagination
-    const existingRecord = data.records.find(
-        (r) => r.name === hostname && r.type === 'A',
-    );
+	// Check if the DNS record already exists
+	const GET_DNS = new URL(
+		`/v4/domains/${encodeURIComponent(domain)}/records?teamId=${teamId}&limit=100`,
+		API,
+	);
+	let res = await fetch(GET_DNS, { headers });
+	if (!res.ok) {
+		return 'badauth';
+	}
+	const data = await res.json();
 
-    if (existingRecord) {
-        //console.log('found existing record');
+	// TODO: handle pagination
+	const existingRecord = data.records.find(
+		(r) => r.name === hostname && r.type === 'A',
+	);
 
-        if (existingRecord.value === ip) {
-            //console.log('no change');
-            return `nochg ${ip}`;
-        }
+	headers.set('Content-Type', 'application/json; charset=utf-8');
+	const body = {
+		type: 'A',
+		name: hostname,
+		value: ip,
+		ttl: existingRecord?.ttl || 60,
+		comment: existingRecord?.comment,
+	};
 
-        const PATCH_DNS = new URL(
-            `/v1/domains/records/${encodeURIComponent(existingRecord.id)}?teamId=${teamId}`,
-            API,
-        );
-        const body = {
-            type: 'A',
-            name: hostname,
-            value: ip,
-            ttl: existingRecord.ttl || 60,
-            comment: existingRecord.comment,
-        };
-        const res = await fetch(PATCH_DNS, {
-            method: 'PATCH',
-            headers: {
-                Authorization: `Bearer ${token}`,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body),
-        });
-        //console.log(await res.json());
-    } else {
-        //console.log('creating new record');
-        const POST_DNS = new URL(
-            `/v2/domains/${encodeURIComponent(domain)}/records?teamId=${teamId}`,
-            API,
-        );
-        const body = {
-            type: 'A',
-            name: hostname,
-            value: ip,
-            ttl: 60,
-        };
-        const res = await fetch(POST_DNS, {
-            method: 'POST',
-            headers: {
-                Authorization: `Bearer ${token}`,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body),
-        });
-        //console.log(await res.json());
-    }
-    return `good ${ip}`;
+	if (existingRecord) {
+		if (existingRecord.value === ip) {
+			console.log(
+				`no change to existing record for ${hostname}.${domain}: ${ip}`,
+			);
+			return `nochg ${ip}`;
+		}
+
+		console.log(`updating existing record for ${hostname}.${domain}: ${ip}`);
+		const PATCH_DNS = new URL(
+			`/v1/domains/records/${encodeURIComponent(existingRecord.id)}?teamId=${teamId}`,
+			API,
+		);
+		res = await fetch(PATCH_DNS, {
+			method: 'PATCH',
+			headers,
+			body: JSON.stringify(body),
+		});
+	} else {
+		console.log(`creating new record for ${hostname}.${domain}: ${ip}`);
+		const POST_DNS = new URL(
+			`/v2/domains/${encodeURIComponent(domain)}/records?teamId=${teamId}`,
+			API,
+		);
+		res = await fetch(POST_DNS, {
+			method: 'POST',
+			headers,
+			body: JSON.stringify(body),
+		});
+	}
+
+	if (!res.ok) {
+		const text = await res.text();
+		console.log(
+			`failed to update record for ${hostname}.${domain}: ${ip}: ${text}`,
+		);
+		return 'dnserr';
+	}
+
+	return `good ${ip}`;
 }
